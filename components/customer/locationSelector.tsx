8982a7c43865cdb57ca66c92030b2f23d3e96790
Vijay Babu <80508144+Vijay-0405@users.noreply.github.com>
2025-10-01 12:34:05 +0530

    more css changes

--- components/customer/locationSelector.tsx
+++ components/customer/locationSelector.tsx
@@ -112,22 +112,31 @@ export default function LocationSelector({onSelectedAddressChange} : LocationSel
 
   const [isPopupOpen,setIsPopupOpen] = useState<boolean>(false);
   const handlePopupCancel = () => {
+    //resetting the address form details
+    setAddressFormDetails({});
     setIsPopupOpen(false);
   };
   const handlePrimaryAction = async () => {
-    try{
-      setLoading(true);
-      const createAddress = await api.post("/user/address/add", addressFormDetails);
-      //closing the popup
-      setIsPopupOpen(false);
-      setLoading(false);
+    if (addressFormDetails?.contact_name && addressFormDetails?.contact_number &&
+        addressFormDetails?.address && addressFormDetails?.location_name &&
+        addressFormDetails?.latitude && addressFormDetails?.longitude
+    ) {
+      try{
+        setLoading(true);
+        const createAddress = await api.post("/user/address/add", addressFormDetails);
+        //closing the popup
+        setIsPopupOpen(false);
+        setLoading(false);
 
-      //TODO set the created address id as selected Id
-      //fetching and assiging the addresses again
-      fetchAndSetAddresses();
-    } catch(error) {
-       setLoading(false);
-      //TODO error handling
+        //TODO set the created address id as selected Id
+        //fetching and assiging the addresses again
+        fetchAndSetAddresses();
+      } catch(error) {
+        setLoading(false);
+        //TODO error handling
+      }
+    } else {
+      alert("Please provide all the required details");
     }
   };
 
@@ -214,37 +223,41 @@ export default function LocationSelector({onSelectedAddressChange} : LocationSel
        <FullScreenLoader loading={loading}/>
        <PopupModel open={isPopupOpen} onCancel={handlePopupCancel} onPrimary={handlePrimaryAction} primaryLabel="Add">
         <form className="w-full max-w-lg bg-white rounded-xl px-8 py-10 shadow-lg">
-          <h2 className="text-2xl font-bold mb-8 text-center">Enter Address Details</h2>
-          <div className="mb-3">
+          <h2 className="text-base font-bold mb-8 text-center">Enter Address Details</h2>
+          <div className="mb-3 text-sm">
             <label htmlFor="contact_name" className="block font-semibold mb-1">Contact Name *</label>
             <input
               type="text"
               id="contact_name"
               name="contact_name"
-              value={addressFormDetails?.contact_name}
+              value={addressFormDetails?.contact_name || ""}
               onChange={handleAddressDetailsChange}
               required
               className="w-full px-3 py-2 rounded-md border border-gray-300 bg-gray-50 focus:outline-none"
             />
           </div>
-          <div className="mb-3">
+          <div className="mb-3 text-sm">
             <label htmlFor="contact_number" className="block font-semibold mb-1">Contact Number *</label>
             <input
               type="tel"
               id="contact_number"
               name="contact_number"
-              value={addressFormDetails?.contact_number}
-              onChange={handleAddressDetailsChange}
+              value={addressFormDetails?.contact_number || ""}
+              onChange={(e) => {
+                      const value = e.target.value.replace(/\D/g, "");
+                      e.target.value = value;
+                      if (value.length <= 10) handleAddressDetailsChange(e);
+                    }}
               required
               className="w-full px-3 py-2 rounded-md border border-gray-300 bg-gray-50 focus:outline-none"
             />
           </div>
-          <div className="mb-3">
+          <div className="mb-3 text-sm">
             <label htmlFor="address" className="block font-semibold mb-1">Address *</label>
             <textarea
               id="address"
               name="address"
-              value={addressFormDetails?.address}
+              value={addressFormDetails?.address || ""}
               onChange={handleAddressDetailsChange}
               required
               className="w-full px-3 py-2 rounded-md border border-gray-300 bg-gray-50 focus:outline-none"
@@ -263,19 +276,19 @@ export default function LocationSelector({onSelectedAddressChange} : LocationSel
               placeholder="Flat, Landmark, etc."
             />
           </div> */}
-          <div className="mb-3">
+          <div className="mb-3 text-sm">
             <label htmlFor="location_name" className="block font-semibold mb-1">Location Name *</label>
             <input
               type="text"
               id="location_name"
               name="location_name"
-              value={addressFormDetails?.location_name}
+              value={addressFormDetails?.location_name || ""}
               onChange={handleAddressDetailsChange}
               required
               className="w-full px-3 py-2 rounded-md border border-gray-300 bg-gray-50 focus:outline-none"
             />
           </div>
-          <div>
+          <div className="mb-3 text-sm">
             <MapSelector onChange={(lat, lng) => setAddressFormDetails(prev => ({
               ...prev,
               latitude: lat,
