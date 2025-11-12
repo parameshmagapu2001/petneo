f9c3cb952d9f84307a87be81b657296fd503c1ff
Vijay Babu <80508144+Vijay-0405@users.noreply.github.com>
2025-11-12 22:44:50 +0530

    dashboard page initial changes

--- src/app/partner/dashboard/page.tsx
+++ src/app/partner/dashboard/page.tsx
@@ -1,11 +1,106 @@
 "use client";
+import {FaCirclePlus} from "react-icons/fa6";
+import React, {useEffect, useRef, useState} from "react";
+import {api} from "@/utils/api";
+import {PartnerDetails} from "@/app/partner/layout";
+import FullScreenLoader from "../../../../components/customer/fullScreenLoader";
+
+interface ProgressBarProps {
+    percentage: number;
+}
+
+function ProgressBar ({ percentage }: ProgressBarProps) {
+    // Clamp value between 0 and 100 for safety
+    const safePercentage = Math.max(0, Math.min(percentage, 100));
+    return (
+        <div className="flex items-center w-full">
+            <div className="w-full bg-pink-100 rounded-full h-3 relative">
+                <div
+                    className="bg-pink-500 h-3 rounded-full"
+                    style={{ width: `${safePercentage}%` }}
+                />
+            </div>
+            <span className="ml-3 text-black text-base font-medium">
+        {safePercentage}%
+      </span>
+        </div>
+    );
+};
 
 export default function PartnerDashboard()  {
+
+    const [partnerDetails, setPartnerDetails] = useState<PartnerDetails>({});
+    const hasFetched = useRef(false);
+    const [loading, setLoading] = useState<boolean>(true);
+    useEffect(() => {
+        if (!hasFetched.current) {
+            hasFetched.current = true;
+            const vetTodaySummary = api.get("/appointments/vetTodaySummary");
+            Promise.all([vetTodaySummary]).then(([vetTodaySummaryRes]) => {
+                //setting the partner data
+                setPartnerDetails(vetTodaySummaryRes)
+
+                hasFetched.current = false;
+                setLoading(false);
+            }).catch((error) => {
+                setLoading(false);
+                //TODO handle error cases
+            })
+        }
+    }, []);
+
     return (
         <>
-            <div>
-                sample page content
+            <div className="px-6 py-6 max-w-7xl mx-auto space-y-10">
+                {/* Greeting & My Pets */}
+                <section className="bg-white rounded-lg shadow-md p-6 flex justify-between items-center">
+                    <div className="flex items-center space-x-4">
+                        <img
+                            src="../images/customer/paws.png"
+                            alt="paws"
+                        />
+                        <div>
+                            <h2 className="text-lg font-semibold">Hello, Dr. {partnerDetails?.vet_name}</h2>
+                            <p className="text-gray-500 text-sm">
+                                Let's get started from where we left.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="flex flex-col w-[40%]">
+                        <h3 className="text-pink-600 font-semibold mb-1 select-none">My Appointments</h3>
+                        <span className="text-sm font-medium mb-1 select-none">{`${partnerDetails?.completed}/${partnerDetails?.total_appointments} Appointments Completed`}</span>
+                        <div className="flex items-center gap-1 cursor-pointer group mb-4 cursor-pointer">
+                            <span className="text-gray-500 text-xs font-medium group-hover:text-gray-700 transition-colors">
+                                See All
+                            </span>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-3 w-3 text-gray-500 group-hover:text-gray-700 transition-colors"
+                                fill="none"
+                                viewBox="0 0 24 20"
+                                stroke="currentColor"
+                                strokeWidth={2}
+                            >
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
+                            </svg>
+                        </div>
+                        {partnerDetails?.completed && partnerDetails.completed > 0  &&
+                            partnerDetails?.total_appointments && partnerDetails.total_appointments > 0 ?
+                            <ProgressBar percentage={Math.round(partnerDetails.completed/partnerDetails.total_appointments * 100)} /> : ""}
+                    </div>
+
+                    {/* Illustration */}
+                    <div className="hidden md:block self-start">
+                        <img
+                            src="../images/customer/calender.png"
+                            alt="Pet management illustration"
+                            className="w-28 h-auto select-none pointer-events-none"
+                            draggable={false}
+                        />
+                    </div>
+                </section>
             </div>
+            <FullScreenLoader loading={loading}/>
         </>
     );
 }
