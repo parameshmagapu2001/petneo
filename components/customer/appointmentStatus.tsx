ee899e25fcd25962d45a8e8e5e6994507031f26f
Vijay Babu <80508144+Vijay-0405@users.noreply.github.com>
2025-11-13 10:36:42 +0530

    user reschedule changes

--- components/customer/appointmentStatus.tsx
+++ components/customer/appointmentStatus.tsx
@@ -1,11 +1,19 @@
 import { Pet } from '@/app/customer/dashboard/page';
 import { PageType } from "@/app/customer/dashboard/constants";
 import { api } from '@/utils/api';
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import { FaCheckCircle, FaCalendarAlt, FaMapMarkerAlt, FaDog, FaCut } from 'react-icons/fa';
 import { FcCancel } from 'react-icons/fc';
 import { LiaPawSolid } from "react-icons/lia";
 import FullScreenLoader from './fullScreenLoader';
+import PopupModel from "./popupModel";
+import SlotPicker, {DaySlots} from "./slotPicker";
+import {
+    convert12hTo24hPlusMinutes,
+    DateTimeSlot,
+    defaultNumberOfDays, defaultTimeSlotInMin,
+    transformAvailability, VISIT_TYPES
+} from "./cVetAppointmentBooking";
 
 export type AppointmentStatusType = 'booked' | 'cancelled' | 'completed';
 export interface AppointmentDetails {
@@ -46,7 +54,10 @@ function formatDate(inputDate: string | undefined): string {
 
 export default function AppointmentStatus({appointmentDetails, onPageTypeChange, makeSelectedAppointmentEmpty} : AppointmentStatusProps) {
 
-  const [appointmentStatus, setAppointmentStatus] = useState<AppointmentStatusType>(appointmentDetails.status);
+    const [appointmentDate, setAppointmentDate] = useState<string>(appointmentDetails.date || "");
+    const [appointmentTime, setAppointmentTime] = useState<string>(appointmentDetails.time || "");
+
+    const [appointmentStatus, setAppointmentStatus] = useState<AppointmentStatusType>(appointmentDetails.status);
 
   const [loading, setLoading] = useState<boolean>(false);
 
@@ -59,6 +70,76 @@ export default function AppointmentStatus({appointmentDetails, onPageTypeChange,
       setLoading(false);
     } 
   }
+
+  const [isReschedulePopupOpen, setIsReschedulePopupOpen] = useState<boolean>(false);
+    function handleRescheduleAppointment(): void {
+        if (appointmentDetails.id) {
+           setIsReschedulePopupOpen(true);
+        }
+    }
+
+    const handlePopupCancel = () => {
+        setIsReschedulePopupOpen(false);
+        setRescheduleAvailability([]);
+        setSelectedDateTimeSlot(undefined);
+    };
+
+    const handlePrimaryAction = async () => {
+        if (selectedDateTimeSlot && selectedDateTimeSlot.date &&
+            selectedDateTimeSlot.time) {
+            try {
+                setLoading(true);
+                const payload= {
+                    new_date: selectedDateTimeSlot.date,
+                    new_start_time: convert12hTo24hPlusMinutes(selectedDateTimeSlot.time),
+                    new_end_time: convert12hTo24hPlusMinutes(selectedDateTimeSlot.time, defaultTimeSlotInMin),
+                    visit_type: VISIT_TYPES.find((item) => item.displayName === appointmentDetails.visitType)?.id,
+                };
+
+                //reschedule the appointment
+                const rescheduleAppointmentRes = await api.put(`/availability/user/reschedule/${appointmentDetails.id}`, payload);
+                setLoading(false);
+                if (rescheduleAppointmentRes.success) {
+                    //assign the new data to current appointment
+                    setAppointmentDate(selectedDateTimeSlot.date);
+                    setAppointmentTime(selectedDateTimeSlot.time);
+                }
+            } catch (e) {
+                //TODO error scenario
+            } finally {
+                setLoading(false);
+                setIsReschedulePopupOpen(false);
+            }
+        } else {
+            alert("Please choose a slot to reschedule");
+        }
+    }
+
+    const [selectedDateTimeSlot, setSelectedDateTimeSlot] = useState<DateTimeSlot>();
+    function handleSlotPickerValueChange(selected: { date: string; time: string; }): void {
+        setSelectedDateTimeSlot(selected);
+    }
+
+    const [rescheduleAvailability, setRescheduleAvailability] = useState<DaySlots[]>([]);
+
+    useEffect(() => {
+        (async () => {
+            if (isReschedulePopupOpen && appointmentDetails.id) {
+                try {
+                    //fetching the reschedule slots.
+                    setLoading(true);
+                    const fetchRescheduleSlots = await api.get(`/availability/${appointmentDetails.id}/rescheduleSlots`, {days: defaultNumberOfDays});
+                    if (Array.isArray(fetchRescheduleSlots)) {
+                        setRescheduleAvailability(transformAvailability(fetchRescheduleSlots));
+                    }
+                } catch (e) {
+                    //TODO error handling scenario.
+                } finally {
+                    setLoading(false);
+                }
+            }
+        })();
+    }, [isReschedulePopupOpen]);
 
   function handleGoHome(): void {
     //navigating to the dashboard home
@@ -78,7 +159,8 @@ export default function AppointmentStatus({appointmentDetails, onPageTypeChange,
           <>
             <FaCheckCircle className="text-pink-400 text-6xl mb-4" />
             <h2 className="font-semibold text-lg mb-2">Appointment Booked.</h2>
-            <span className="font-medium mb-4">{appointmentDetails.vetName ? "Dr. " + appointmentDetails.vetName : ""}</span>
+            <span className="font-medium mb-4">{appointmentDetails.vetName ?
+                appointmentDetails.vetName.includes("Dr.") ? appointmentDetails.vetName : "Dr. " + appointmentDetails.vetName : ""}</span>
           </>
         )}
         { appointmentStatus === "cancelled" && (
@@ -92,7 +174,8 @@ export default function AppointmentStatus({appointmentDetails, onPageTypeChange,
           <>
             <FaCheckCircle className="text-pink-400 text-6xl mb-4" />
             <h2 className="font-semibold text-lg mb-2">Appointment Completed.</h2>
-            <span className="font-medium mb-4">{appointmentDetails.vetName ? "Dr. " + appointmentDetails.vetName : ""}</span>
+            <span className="font-medium mb-4">{appointmentDetails.vetName ?
+                appointmentDetails.vetName.includes("Dr.") ? appointmentDetails.vetName : "Dr. " + appointmentDetails.vetName : ""}</span>
           </>
         )}
 
@@ -119,7 +202,7 @@ export default function AppointmentStatus({appointmentDetails, onPageTypeChange,
             <div className="px-2">
               <FaCalendarAlt className="mr-2 text-gray-500" size={20} />
             </div>
-            <span className="font-medium">{formatDate(appointmentDetails.date)} - {appointmentDetails.time}</span>
+            <span className="font-medium">{formatDate(appointmentDate)} - {appointmentTime}</span>
           </div>
           <div className="flex items-center border-x-2 border-b-2 border-[#B7B7B7] py-3 rounded-b-lg bg-white">
             <div className="px-2">
@@ -140,10 +223,17 @@ export default function AppointmentStatus({appointmentDetails, onPageTypeChange,
                 </li>
               </ul>
             </div>
-            <button className="w-full text-white bg-pink-500 rounded-lg py-3 my-3 font-semibold transition hover:bg-pink-600"
-            onClick={handleCancelAppointment}>
-              Cancel Appointment
-            </button>
+              <div className="w-full mb-6 flex justify-between">
+                  <button className="w-[40%] text-white bg-pink-500 rounded-lg py-3 my-3 font-semibold transition hover:bg-pink-600"
+                          onClick={handleCancelAppointment}>
+                      Cancel
+                  </button>
+                  <button className="w-[40%] text-white bg-pink-500 rounded-lg py-3 my-3 font-semibold transition hover:bg-pink-600"
+                          onClick={handleRescheduleAppointment}>
+                      Reschedule
+                  </button>
+              </div>
+
             <button className="w-full text-white bg-pink-500 rounded-lg py-3 my-3 font-semibold transition hover:bg-pink-600"
             onClick={handleViewMyAppointments}>
               View My Appointments
@@ -157,6 +247,9 @@ export default function AppointmentStatus({appointmentDetails, onPageTypeChange,
           </button>
         )}
       </div>
+        <PopupModel open={isReschedulePopupOpen} onCancel={handlePopupCancel} onPrimary={handlePrimaryAction} primaryLabel="Reschedule">
+            <SlotPicker vetAvailability={rescheduleAvailability} onChange={handleSlotPickerValueChange}/>
+        </PopupModel>
       <FullScreenLoader loading={loading}/>
     </div>
   );
